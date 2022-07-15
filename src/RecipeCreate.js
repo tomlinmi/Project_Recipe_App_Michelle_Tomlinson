e51f0be62edfd665b28f54e11ca2a95626c709fa
M Tomlinson <47996533+tomlinmi@users.noreply.github.com>
2022-07-15 12:16:20 -0400

    Add files via upload

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -1,93 +1,87 @@
-import React, { useState } from "react";
-import "./App.css";
-function RecipeCreate({recipes, setRecipes, createRecipe}) {
+import React, { useState } from "react"; 
+function RecipeCreate({createRecipe}) { 
+    // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared. 
+    // TODO: Add the required input and textarea form elements. 
+    // TODO: Add the required submit and change handlers 
+const initialFormState = { 
+    name:"", 
+    cuisine:"", 
+    photo:"", 
+    ingredients:"", 
+    preparation:"", 
+}; 
 
-  // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
-  // TODO: Add the required input and textarea form elements.
-  // TODO: Add the required submit and change handlers
-  
-  const initialFormState = {
-   name: "Name",
-   cuisine:"Cuisine",
-   photo: "URL",
-   ingredients:"Ingredients",
-   preparation:"Preparation"
- };
- 
-  const [content, setContent] = useState({...initialFormState}); 
-  const handleChange = ({target}) => { const value = target.value; setContent({ ...content, [target.name]: value, }); }; 
-    
-  const handleSubmit = (event) => { 
-    event.preventDefault(); console.log("Submitted:", content); 
+const [content, setContent] = useState({...initialFormState}); 
+//track the changes to the content field 
+
+const handleChange = ({target}) => { 
+    const value = target.value; setContent({ ...content, [target.name]: value, }); 
+}; 
+const handleSubmit = (event) => { event.preventDefault(); 
+    console.log("Submitted:", content); 
     createRecipe(content); 
-    setContent({...initialFormState}); 
-  };
-      
-  return (
-    <form name="create" onSubmit={handleSubmit}>
-         <table>
-        <tbody>
-          <tr>
+    setContent({...initialFormState});
+ }; 
+ 
+ return ( 
+ <form name="create" onSubmit={handleSubmit}> 
+ <table> 
+    <tbody> 
+        <tr> 
             <td> 
-            <input
-              id="name"
-              type = "text"
-              name = "name"
-              onChange= {handleChange}
-              value = {content.name}
-              />
-            </td>
-             <td> 
-            <input
-              id="cuisine"
-              type = "text"
-              name = "cuisine"
-              onChange= {handleChange}
-              value = {content.cuisine}
-              />
-            </td>
-             <td> 
-            <input
-              id="photo"
-              name = "photo"
-              type = "text"
-              placeholder="URL"
-              onChange= {handleChange}
-              value = {content.photo}
-              />
-            </td>
-            
-             <td> 
-            <textarea
-              id="ingredients"
-              name = "ingredients"
-              type = "text"
-              placeholder="Ingredients"
-              onChange= {handleChange}
-              value = {content.ingredients}
-              />
-            </td>
-            
-             <td> 
-            <textarea
-              id="preparation"
-              name = "preparation"
-              type = "text"
-              placeholder="Preparation"
-              onChange= {handleChange}
-              value = {content.preparation}
-              />
-            </td>          
-            
-            <td>
-              <button type="submit">Create</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </form>
-  );
-}
-
+                <input 
+                id="name" 
+                name="name" 
+                type="text" 
+                placeholder="Name" 
+                onChange={handleChange} 
+                value={content.name} 
+                /> 
+                </td> 
+                <td> 
+                    <input 
+                    id="cuisine" 
+                    name="cuisine" 
+                    type="text" 
+                    placeholder="Cuisine" 
+                    onChange={handleChange} 
+                    value={content.cuisine} 
+                    /> 
+                    </td> 
+                    <td> 
+                        <input id="photo" 
+                        name="photo" 
+                        type="text" 
+                        placeholder="Photo" 
+                        onChange={handleChange} 
+                        value={content.photo} 
+                        /> 
+                        </td> 
+                        <td> 
+                            <textarea 
+                            id="ingredients" 
+                            name="ingredients" 
+                            type="text" 
+                            placeholder="Ingredients" 
+                            onChange={handleChange} 
+                            value={content.ingredients} 
+                            /> 
+                            </td> 
+                            <td> 
+                                <textarea 
+                                id="preparation" 
+                                name="preparation" 
+                                type="text" 
+                                placeholder="Preparation" 
+                                onChange={handleChange} 
+                                value={content.preparation} 
+                                /> 
+                                </td> 
+                                <td> <button type="submit">Create</button> </td>
+                                 </tr> 
+                                 </tbody> 
+                                 </table> 
+                                 </form> 
+                                 );
+} 
 export default RecipeCreate;
-   
---
+++ src/RecipeView.js
@@ -0,0 +1,23 @@
+import React from "react"; 
+import "./App.css"; 
+function RecipeView({recipe, index, deleteRecipe }){ 
+  
+  return ( 
+    <div className = "recipe"> 
+      <table> 
+        <tbody> 
+          <tr key={index}> 
+            <td><p>{recipe.name}</p></td> 
+            <td><p>{recipe.cuisine}</p></td> 
+            <td><img src={recipe.photo} alt={recipe.photo}></img></td> 
+            <td className="content_td"><p>{recipe.ingredients}</p></td> 
+            <td className="content_td"><p>{recipe.preparation}</p></td> 
+            <td><button name = "delete" onClick={deleteRecipe}>Delete</button></td> 
+          </tr>
+        </tbody>
+      </table>
+    </div> ) 
+}; 
+
+
+export default RecipeView;
